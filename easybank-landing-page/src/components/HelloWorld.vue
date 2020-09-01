ec3c04d049155cef31c63959ee89325d8cad907a
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2020-09-01 16:25:53 +0100

    added tailwind css to vue project

---
+++ easybank-landing-page/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello bg-blue">
+    <h1>{{ msg }}</h1>
+    <p class="text-center">
+      This is a tailwind css test.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
